5fcc8362534aa20637e8ec434b9f27fd1259734d
singhgag <singhgag@iu.edu>
2020-07-30 01:24:51 -0400

    basic ajax setup + other configuration housekeeping (work-in-progress)

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Ping msg="Hello world ping!!"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
+import Ping from '@/components/Ping.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Ping,
   },
 };
 </script>
